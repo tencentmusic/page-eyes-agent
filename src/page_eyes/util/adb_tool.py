c1439683696fb18e4af2bcce6c9d5de8a9afc5cb
aidenmo <aidenmo@tencent.com>
2025-12-25 11:09:03 +0800

    release: 1.1.4

--- src/page_eyes/util/adb_tool.py
+++ src/page_eyes/util/adb_tool.py
@@ -12,22 +12,22 @@ from loguru import logger
 class AdbDeviceProxy:
     def __init__(self, device: AdbDevice):
         self._device = device
-        self.adb_target_dir = "/data/local/tmp"
-        self.adb_path = self._push_yadb()
+        self.y_adb_path = Path("/data/local/tmp/yadb")
+        self._push_yadb()
 
     def _push_yadb(self):
         if not getattr(self._device, 'yadb', None):
-            adb_bin_path = Path(__file__).parent / 'bin' / 'yadb'
-            logger.info('push yadb to device...')
-            self._device.sync.push(src=adb_bin_path, dst=self.adb_target_dir)
-            setattr(self._device, 'yadb', f'{self.adb_target_dir}/yadb')
-        return getattr(self._device, 'yadb')
+            if not self._device.sync.exists(str(self.y_adb_path)):
+                adb_bin_path = Path(__file__).parent / 'bin' / 'yadb'
+                logger.info('push yadb to device...')
+                self._device.sync.push(src=adb_bin_path, dst=self.y_adb_path)
+            setattr(self._device, 'yadb', self.y_adb_path)
 
     def execute_command(self, *args: str):
         self._device.shell([
             'app_process',
-            f'-Djava.class.path={self.adb_path}',
-            f'{self.adb_target_dir}',
+            f'-Djava.class.path={self.y_adb_path}',
+            f'{self.y_adb_path.parent}',
             'com.ysbing.yadb.Main',
             *args
         ])
