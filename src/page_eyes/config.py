7fae830b3e82b89e8c5c24a151e382bb6cf70eea
aidenmo <aidenmo@tencent.com>
2026-03-17 10:29:54 +0800

    feat: 更新配置默认值

--- src/page_eyes/config.py
+++ src/page_eyes/config.py
@@ -39,7 +39,7 @@ class MinioConfig(BaseSettings):
 class BrowserConfig(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_prefix='browser_', extra='ignore')
 
-    headless: Optional[bool] = True
+    headless: Optional[bool] = False
     simulate_device: Optional[Literal['iPhone 15', 'iPhone 15 Pro', 'iPhone 15 Pro Max', 'iPhone 6'] | str] = None
 
 
